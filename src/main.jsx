e262a81134089efeb937fae6c2109da8f38baea8
Rail-prog <vasivancea@icloud.com>
2025-10-18 17:08:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from 'react-router-dom';
-import posts from './Post.js';
+import posts from './posts.js';
 
 export default function Post() {
   const { slug } = useParams();
