025c9a1324c378c6e5f734296d4e02e53e4e7b9d
Rail-prog <vasivancea@icloud.com>
2025-10-19 09:05:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
-// If you have Tailwind/global CSS, uncomment:
-// import "./index.css";
+import Blog from "./Blog.jsx";
+import Post from "./Post.jsx";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/post/:slug" element={<Post />} />
+      {/* Fallback to home */}
+      <Route path="*" element={<App />} />
+    </Routes>
   </BrowserRouter>
 );
+
 
