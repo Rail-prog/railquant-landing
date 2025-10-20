32a3db8063c650066a4406051634828364293efa
Rail-prog <vasivancea@icloud.com>
2025-10-20 16:51:09 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
-// src/main.jsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";   // <-- FIXED: no "components/" prefix
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Blog from "./Blog.jsx";
+import Post from "./Post.jsx";
+import Success from "./Success.jsx";
+import Cancel from "./Cancel.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/post/:slug" element={<Post />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 
