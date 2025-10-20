024a403330a1f357a90ce3ff0fef61a23fef4a74
Rail-prog <vasivancea@icloud.com>
2025-10-20 18:48:33 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,16 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import Blog from "./Blog.jsx";
-import Post from "./Post.jsx";
-import Success from "./Success.jsx";
-import Cancel from "./Cancel.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/post/:slug" element={<Post />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />
-        <Route path="*" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
-
-
-
 
 
 
