d8b97bbd90f6bc4f22b880b8961146b47d619f54
Rail-prog <vasivancea@icloud.com>
2025-10-20 21:22:48 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
 
-// IMPORTANT: this line makes Tailwind load
+// This import is what makes Tailwind styles appear
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
+
 
 
 
