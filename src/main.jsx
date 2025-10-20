a5b8987114596fb82cd307e012d6ae8667b6ae76
Rail-prog <vasivancea@icloud.com>
2025-10-20 20:41:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css"; // ← IMPORTANT: brings in Tailwind
+
+// IMPORTANT: this line makes Tailwind load
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+
 
 
 
