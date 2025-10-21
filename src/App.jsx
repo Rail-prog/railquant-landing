9d14d3a73a676e8c332376d7f45ae490a05f6c73
Rail-prog <vasivancea@icloud.com>
2025-10-21 06:00:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from "react-router-dom";
-import posts from "./posts.js";
+import posts from "./posts.js"; 
 
 export default function App() {
   const latest = [...posts]
