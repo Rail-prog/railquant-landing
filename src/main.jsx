0a2220f917f2c1510a4ab01b3dcb0b302c2a06f2
Rail-prog <vasivancea@icloud.com>
2025-10-22 07:42:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,21 @@ import Blog from "./Blog.jsx";
 import Post from "./Post.jsx";
 import Privacy from "./Privacy.jsx";
 import Terms from "./Terms.jsx";
-// If you also have success/cancel pages, import them too:
-// import Success from "./Success.jsx";
-// import Cancel from "./Cancel.jsx";
+import Success from "./Success.jsx";
+import Cancel from "./Cancel.jsx";
+
+// ...
+<Routes>
+  <Route path="/" element={<App />} />
+  <Route path="/blog" element={<Blog />} />
+  <Route path="/post/:slug" element={<Post />} />
+  <Route path="/privacy" element={<Privacy />} />
+  <Route path="/terms" element={<Terms />} />
+  <Route path="/success" element={<Success />} />
+  <Route path="/cancel" element={<Cancel />} />
+  <Route path="*" element={<App />} />
+</Routes>
+
 
 import "./index.css";
 
